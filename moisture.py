c3e656239319c5de9a2f2d08278cadb54eb79276
Jake Webb <jakewebbsite@gmail.com>
2019-05-02 16:34:46 -0500

    add gpio callback

--- moisture.py
+++ moisture.py
@@ -25,7 +25,7 @@ channel = 17
 GPIO.setup(channel, GPIO.IN)
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-# GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_detect(channel, GPIO.BOTH, callback, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
 # GPIO.add_event_callback(channel, callback)
 
