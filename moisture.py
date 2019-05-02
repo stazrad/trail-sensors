a8b9f7a5b92f188361d8361c4c7af76c2af775db
Jake Webb <jakewebbsite@gmail.com>
2019-05-02 16:27:02 -0500

    more logs

--- moisture.py
+++ moisture.py
@@ -25,12 +25,12 @@ channel = 17
 GPIO.setup(channel, GPIO.IN)
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+# GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-GPIO.add_event_callback(channel, callback)
+# GPIO.add_event_callback(channel, callback)
 
 # This is an infinte loop to keep our script running
 while True:
-    print(GPIO.input(17))
+    print("PIN 17", GPIO.input(17))
     # This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
     time.sleep(0.1)
