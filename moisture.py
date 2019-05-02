22c4c8f10e67ae63453e2c9e718646cdbf8140a1
Jake Webb <jakewebbsite@gmail.com>
2019-05-02 16:50:20 -0500

    print GPIO

--- moisture.py
+++ moisture.py
@@ -4,14 +4,13 @@
 
 import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
 import time # This is the time library, we need this so we can use the sleep function
-import spidev
 
 print "file running!"
 
 # This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
 
 def callback(channel):
-    print(GPIO.input(channel))
+    print(GPIO)
     if GPIO.input(channel):
         print "LED off"
     else:
@@ -26,22 +25,12 @@ channel = 17
 GPIO.setup(channel, GPIO.IN)
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-GPIO.add_event_detect(channel, GPIO.BOTH, callback, bouncetime=300)
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-# GPIO.add_event_callback(channel, callback)
-
-delay = 0.2
-
-spi = spidev.SpiDev()
-spi.open(0,0)
-
-def readChannel(channel):
-  val = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((val[1]&3) << 8) + val[2]
-  return data
+GPIO.add_event_callback(channel, callback)
 
 # This is an infinte loop to keep our script running
 while True:
-    print("PIN 17", readChannel(channel))
+    # print("PIN 17", GPIO.input(17))
     # This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
     time.sleep(0.1)
